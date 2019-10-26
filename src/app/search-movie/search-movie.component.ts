74b4a540e4a8b23776fd5b2657562949360a766b
Balancedsan <johnwee35@gmail.com>
2019-10-26 21:25:01 +0800

    john refactor to mergemap and check movie result

--- src/app/search-movie/search-movie.component.ts
+++ src/app/search-movie/search-movie.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MoviesService } from '../service/movies.service';
+import { pipe } from 'rxjs';
+import { mergeMap } from "rxjs/operators";
+import Swal from 'sweetalert2'
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-search-movie',
@@ -9,18 +13,35 @@ import { MoviesService } from '../service/movies.service';
 })
 export class SearchMovieComponent implements OnInit {
 
-  constructor(private _route: ActivatedRoute, private _movieService: MoviesService) { }
+  constructor(private _route: ActivatedRoute, private _movieService: MoviesService, private _location: Location) { }
 
   movies;
 
   ngOnInit() {
-    this._route.params.subscribe(val => {
-      let query = val.query;
-
-      this._movieService.searchMovie(query).subscribe(val => {
-        this.movies = val;
+    this._route.params.pipe(
+      mergeMap(val => {
+        return this._movieService.searchMovie(val.query)
       })
-    })
+    ).subscribe(
+      val => {
+        this.movies = val;
+        if (this.movies.length) {
+          Swal.fire({
+            title: `Search Successful, searched ${this.movies.length} movies`,
+            type: "success"
+          })
+        } else {
+          this._location.back();
+          Swal.fire({
+            title: "Your search returned no results",
+            type: "error"
+          })
+        }
+
+      })
+
   }
 
 }
+
+
